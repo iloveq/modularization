6b071c9691b7e7c34f66edc746731db6e6bc6041
woaigmz <woaigmz@gmail.com>
2018-07-27 12:41:24 +0800

    fix some change for tab

--- module-main/src/main/java/com/credithc/hhr/module_main/view/activity/MainActivity.java
+++ module-main/src/main/java/com/credithc/hhr/module_main/view/activity/MainActivity.java
@@ -5,14 +5,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.credithc.hhr.library_common.config.ARouterConstant;
 import com.credithc.hhr.module_main.R;
 import com.credithc.hhr.module_main.R2;
+import com.credithc.hhr.module_main.view.widget.TabView;
 import com.woaiqw.base.common.BaseActivity;
 
 import butterknife.BindView;
@@ -26,18 +25,18 @@ import butterknife.OnClick;
 public class MainActivity extends BaseActivity {
 
     private Fragment[] fragments;
-    private LinearLayout[] mTabs;
+    private TabView[] mTabs;
     private int currentTabIndex;
     @BindView(R2.id.fragment_container)
     FrameLayout fragmentContainer;
     @BindView(R2.id.llAllTab)
     LinearLayout llAllTab;
-    @BindView(R2.id.llTabHome)
-    LinearLayout tabHome;
-    @BindView(R2.id.llTabProject)
-    LinearLayout tabProject;
-    @BindView(R2.id.llTabMine)
-    LinearLayout tabMine;
+    @BindView(R2.id.tvTabHome)
+    TabView tabHome;
+    @BindView(R2.id.tvTabProject)
+    TabView tabProject;
+    @BindView(R2.id.tvTabMine)
+    TabView tabMine;
 
 
     @Override
@@ -47,7 +46,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void afterCreate(Bundle bundle) {
-        initTabButton();
+        mTabs = new TabView[]{tabHome, tabProject, tabMine};
+        tabHome.setChecked(true);
         Fragment homeFragment = (Fragment) ARouter.getInstance().build(ARouterConstant.home_fragment_router_path).navigation();
         Fragment projectFragment = (Fragment) ARouter.getInstance().build(ARouterConstant.project_fragment_router_path).navigation();
         Fragment mineFragment = (Fragment) ARouter.getInstance().build(ARouterConstant.mine_fragment_router_path).navigation();
@@ -56,17 +56,6 @@ public class MainActivity extends BaseActivity {
         showFragment(0);
     }
 
-    /**
-     * 初始化Tab
-     */
-    private void initTabButton() {
-
-        mTabs = new LinearLayout[3];
-        mTabs[0] = tabHome;
-        mTabs[1] = tabProject;
-        mTabs[2] = tabMine;
-        mTabs[0].setSelected(true);
-    }
 
     /**
      * 展示/切换Fragment
@@ -75,76 +64,27 @@ public class MainActivity extends BaseActivity {
      */
     private void showFragment(int index) {
         if (currentTabIndex != index) {
-            FragmentTransaction trx = getSupportFragmentManager().beginTransaction();
-            trx.hide(fragments[currentTabIndex]);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.hide(fragments[currentTabIndex]);
             if (!fragments[index].isAdded()) {
-                trx.add(R.id.fragment_container, fragments[index]);
+                ft.add(R.id.fragment_container, fragments[index]);
             }
-            trx.show(fragments[index]).commitAllowingStateLoss();
+            ft.show(fragments[index]).commitAllowingStateLoss();
         }
-        mTabs[currentTabIndex].setSelected(false);
+        mTabs[currentTabIndex].setChecked(false);
         // 把当前tab设为选中状态
-        mTabs[index].setSelected(true);
+        mTabs[index].setChecked(true);
         currentTabIndex = index;
-
-        changeTabColor(currentTabIndex);
-    }
-
-    /**
-     * 切换时变色
-     *
-     * @param index
-     */
-    private void changeTabColor(int index) {
-        for (int i = 0; i < llAllTab.getChildCount(); i++) {
-
-            LinearLayout currentView = (LinearLayout) llAllTab.getChildAt(i);
-            ImageView currentImage = (ImageView) currentView.getChildAt(0);
-            TextView current_TextView = (TextView) currentView.getChildAt(1);
-            switch (i) {
-                case 0:
-                    if (index == i) {
-                        currentImage.setImageResource(R.mipmap.home_ic_tab_home_check);
-                    } else {
-                        currentImage.setImageResource(R.mipmap.home_ic_tab_home_default);
-                    }
-                    break;
-
-                case 1:
-                    if (index == i) {
-                        currentImage.setImageResource(R.mipmap.home_ic_tab_products_check);
-                    } else {
-                        currentImage.setImageResource(R.mipmap.home_ic_tab_products_default);
-                    }
-                    break;
-
-                case 2:
-                    if (index == i) {
-                        currentImage.setImageResource(R.mipmap.home_ic_tab_my_check);
-                    } else {
-                        currentImage.setImageResource(R.mipmap.home_ic_tab_my_default);
-                    }
-                    break;
-                default:
-                    break;
-            }
-
-            if (i == index) {
-                current_TextView.setTextColor(this.getResources().getColor(R.color.main_tab_text_color_select));
-            } else {
-                current_TextView.setTextColor(this.getResources().getColor(R.color.main_tab_text_color_unselect));
-            }
-        }
     }
 
 
-    @OnClick({R2.id.llTabProject, R2.id.llTabMine, R2.id.llTabHome})
+    @OnClick({R2.id.tvTabHome, R2.id.tvTabProject, R2.id.tvTabMine})
     public void onViewClicked(View view) {
-        if (view.getId() == R.id.llTabHome) {
+        if (view.getId() == R.id.tvTabHome) {
             showFragment(0);
-        } else if (view.getId() == R.id.llTabProject) {
+        } else if (view.getId() == R.id.tvTabProject) {
             showFragment(1);
-        } else if (view.getId() == R.id.llTabMine) {
+        } else if (view.getId() == R.id.tvTabMine) {
             showFragment(2);
         }
 
