217bd2045be21d60084d996f77915fe27d8c216c
woaigmz <woaigmz@gmail.com>
2018-08-01 15:04:49 +0800

    make module-main can single run

--- module-main/src/main/java/module/MainApp.java
+++ module-main/src/main/java/module/MainApp.java
@@ -1,5 +1,8 @@
 package module;
 
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.credithc.hhr.module_main.BuildConfig;
 import com.woaiqw.base.common.BaseApp;
 
 /**
@@ -11,5 +14,10 @@ public class MainApp extends BaseApp {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this);
     }
 }
