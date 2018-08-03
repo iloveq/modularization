bfabb67fcbb8ac2da7f6ab5821d3922785942919
woaigmz <woaigmz@gmail.com>
2018-08-03 18:46:05 +0800

    分享功能：) 111

--- sdk-share/src/main/java/com/woaiqw/sdk_share/ShareSdkProxy.java
+++ sdk-share/src/main/java/com/woaiqw/sdk_share/ShareSdkProxy.java
@@ -1,9 +1,28 @@
 package com.woaiqw.sdk_share;
 
+import android.app.Application;
+
 /**
  * Created by haoran on 2018/8/3.
  */
 
 public class ShareSdkProxy {
+
+    private ShareSdkProxy() {
+    }
+
+    private static class Holder {
+        private static final ShareSdkProxy IN = new ShareSdkProxy();
+    }
+
+
+    public static ShareSdkProxy getInstance() {
+        return Holder.IN;
+    }
+
+    public static void init(Application app, String[] appIds) {
+
+    }
+
 
 }
---
+++ sdk-share/src/main/java/com/woaiqw/sdk_share/model/AppId.java
@@ -0,0 +1,11 @@
+package com.woaiqw.sdk_share.model;
+
+/**
+ * Created by haoran on 2018/8/3.
+ */
+
+public class AppId {
+    private String QQ;
+    private String WECHAT;
+    private String WEIBO;
+}
