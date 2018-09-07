6fb14ce9731fbe05b3de11cfe6e9c87424e27aa6
woaigmz <woaigmz@gmail.com>
2018-09-07 10:54:18 +0800

    Constant 修改

--- library-common/src/main/java/com/credithc/hhr/library_common/config/Constant.java
+++ library-common/src/main/java/com/credithc/hhr/library_common/config/Constant.java
@@ -6,11 +6,27 @@ package com.credithc.hhr.library_common.config;
 
 public class Constant {
 
-    /** fragment */
+    /**
+     * package path
+     */
 
-    public static final String fragment_home = "com.credithc.hhr.module_home.view.fragment.HomeFragment";
-    public static final String fragment_project = "com.credithc.hhr.module_project.view.fragment.ProjectFragment";
-    public static final String fragment_mine = "com.credithc.hhr.module_mine.view.fragment.MineFragment";
+    private static String packageName = "com.credithc.hhr.";
+
+    /**
+     * module path
+     */
+
+    private static String module_home = "module_home.";
+    private static String module_project = "module_project.";
+    private static String module_mine = "module_mine.";
+
+    /**
+     * fragment path
+     */
+
+    public static final String fragment_home = packageName + module_home + "view.fragment.HomeFragment";
+    public static final String fragment_project = packageName + module_project + "view.fragment.ProjectFragment";
+    public static final String fragment_mine = packageName + module_mine + "view.fragment.MineFragment";
 
 
     /** activity */
